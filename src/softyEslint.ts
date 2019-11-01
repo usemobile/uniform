6cd3b1af0c3360b89dcccf489d5f112aff5c50f6
hvpaiva <highlander.vilela@usemobile.xyz>
2019-11-01 13:25:06 -0300

    removing uninstalled rules

--- src/softyEslint.ts
+++ src/softyEslint.ts
@@ -119,8 +119,6 @@ module.exports = {
     "react/require-extension": 0,
     "react/sort-comp": 0,
     "react/static-property-placement": 0,
-    "redux-saga/no-yield-in-race": 2,
-    "redux-saga/yield-effects": 2,
     "require-yield": 0
   },
   settings: {
